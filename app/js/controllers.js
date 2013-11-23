5216ceaa38260500ddceb3f8dc01ff9990a7935c
Jacob Schlitz <jschlitz@gmail.com>
2013-11-23 17:56:08 -0500

    cut old code, Added tests.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,9 +3,21 @@
 /* Controllers */
 
 angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
+  controller('InterestCtrl', ['$scope', function($scope) {
+      $scope.interest = {
+        start : 100000.00,
+        rate : 7.2,
+        per : "year",
+        periods : 30,
+        items : []
+      };
+      $scope.interest.calculate = function() {
+        var items = [$scope.interest.start];
 
-  }])
-  .controller('MyCtrl2', [function() {
+        for (var i=0; i < $scope.interest.periods; i++){
+          items.push(items[i] * (1+$scope.interest.rate/100));
+        };
 
+        $scope.interest.items = items;
+      };
   }]);
--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -6,11 +6,28 @@ describe('controllers', function(){
   beforeEach(module('myApp.controllers'));
 
 
-  it('should ....', inject(function() {
-    expect(false).toBe(false);
+  it('should have the right starting values', inject(function($controller) {
+    var scope={},
+        ctrl = $controller('InterestCtrl', {$scope:scope});
+
+    expect(scope.interest.start).toBe(100000);
+    expect(scope.interest.rate).toBe(7.2);
+    expect(scope.interest.per).toBe('year');
+    expect(scope.interest.periods).toBe(30);
+    expect(scope.interest.items.length).toBe(0);
   }));
 
-  it('should ....', inject(function() {
-    //spec body
+  it('should have more items when we calculate', inject(function($controller) {
+    var scope={},
+        ctrl = $controller('InterestCtrl', {$scope:scope});
+
+    expect(scope.interest.items.length).toBe(0);
+    scope.interest.calculate();
+    expect(scope.interest.items.length).toBe(31);
+
+    scope.interest.periods = 10;
+    scope.interest.calculate();
+    expect(scope.interest.items.length).toBe(11);
   }));
+
 });
