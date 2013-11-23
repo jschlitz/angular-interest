e1ccfad5858468cef4da8ce23e76b39c6f7dd702
Jacob Schlitz <jschlitz@gmail.com>
2013-11-23 18:01:12 -0500

    yay, rule of 2 works

--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -7,8 +7,8 @@ describe('controllers', function(){
 
 
   it('should have the right starting values', inject(function($controller) {
-    var scope={},
-        ctrl = $controller('InterestCtrl', {$scope:scope});
+    var scope={};
+    $controller('InterestCtrl', {$scope:scope});
 
     expect(scope.interest.start).toBe(100000);
     expect(scope.interest.rate).toBe(7.2);
@@ -18,8 +18,8 @@ describe('controllers', function(){
   }));
 
   it('should have more items when we calculate', inject(function($controller) {
-    var scope={},
-        ctrl = $controller('InterestCtrl', {$scope:scope});
+    var scope={};
+    $controller('InterestCtrl', {$scope:scope});
 
     expect(scope.interest.items.length).toBe(0);
     scope.interest.calculate();
@@ -29,5 +29,16 @@ describe('controllers', function(){
     scope.interest.calculate();
     expect(scope.interest.items.length).toBe(11);
   }));
+
+  it('should approximately double after 10 years', inject(function($controller) {
+    var scope={};
+    $controller('InterestCtrl', {$scope:scope});
+
+    scope.interest.periods = 10;
+    scope.interest.calculate();
+    expect(scope.interest.items[10]).toBeCloseTo(200423,0);
+  }));
+
+  //200,423.1361654337
 
 });
