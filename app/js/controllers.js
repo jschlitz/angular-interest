020676507a45d093e48aafb9c31aa313d9a86ca1
Jacob Schlitz <jschlitz@gmail.com>
2013-12-02 17:08:49 -0500

    some experiments with updating the list live. Progress, but I don't like it.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -9,22 +9,34 @@ angular.module('myApp.controllers', []).
         additional : 17500+11000,
         rate : 7.2,
         per : "Year",
+        foopy: 'foo',
         periods : 30,
         items : [],
-        intervalRate : function() {
-          return (this.per === "Month") ? this.rate/12 : this.rate;
+        intervalRate : function(x) {
+          return (x.per === "Month") ? x.rate/12 : x.rate;
         }
       };
-
+/*
       $scope.interest.calculate = function() {
         var items = [$scope.interest.start];
 
         for (var i=0; i < $scope.interest.periods; i++){
-          items.push(items[i] * (1+$scope.interest.intervalRate()/100) + $scope.interest.additional);
+          items.push(items[i] * (1+$scope.interest.intervalRate($scope.interest)/100) + $scope.interest.additional);
         };
 
         $scope.interest.items = items;
 
         console.log(items.length);
       };
+*/
+      $scope.interest.calcItems= function(start, periods, intRate, additional) {
+        var items = [start];
+
+        for (var i=0; i < periods; i++){
+          items.push(items[i] * (1+intRate/100) + additional);
+        };
+
+        console.log(items.length);
+        return items;
+      }
   }]);
