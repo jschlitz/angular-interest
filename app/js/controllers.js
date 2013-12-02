f640e69e5f405317f24edf83c5f5b5c2058354bd
Jacob Schlitz <jschlitz@gmail.com>
2013-12-02 12:44:59 -0500

    numeric inputs, new input

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -6,6 +6,7 @@ angular.module('myApp.controllers', []).
   controller('InterestCtrl', ['$scope', function($scope) {
       $scope.interest = {
         start : 100000.00,
+        additional : 17500+11000,
         rate : 7.2,
         per : "Year",
         periods : 30,
@@ -15,7 +16,7 @@ angular.module('myApp.controllers', []).
         var items = [$scope.interest.start];
 
         for (var i=0; i < $scope.interest.periods; i++){
-          items.push(items[i] * (1+$scope.interest.rate/100));
+          items.push(items[i] * (1+$scope.interest.rate/100) + $scope.interest.additional);
         };
 
         $scope.interest.items = items;
